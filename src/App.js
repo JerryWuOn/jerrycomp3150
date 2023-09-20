5bfefded4f8611a23b9917c1db289b2ce3b326ae
Jerrywuon <113319400+JerryWuOn@users.noreply.github.com>
2023-09-20 21:32:31 +0000

    finalize photo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 import { useState } from "react";
+import imageOne from "./Images/penguin.jpg"
+import imageTwo from "./Images/spaghetti.jpeg"
+import imageThree from "./Images/turtle.jpeg"
 
 
 export default function App() {
@@ -7,7 +10,7 @@ export default function App() {
     <div className="App">
       <h1>My News Feed</h1>
       <Avatar
-        image="https://picsum.photos/160/80"
+        image={imageThree}
         header="Animals never eat pizza that they do not like"
         name="Jerry"
         link="https://www.britannica.com/plant/carrot"
@@ -15,7 +18,7 @@ export default function App() {
         information="1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishin"
       />
       <Avatar
-        image="https://files.worldwildlife.org/wwfcmsprod/images/HERO_Penguins_Antarctica/hero_full/29o7jhrgam_Medium_WW267491.jpg/160/80"
+        image={imageOne}
         header="Facts about why we eat spaghetti"
         name="Kiiroitori"
         link="https://www.britannica.com/plant/potato"
@@ -23,7 +26,7 @@ export default function App() {
         information="And their ravenous appetite keeps countless insect pests in check. The spiders serve to protect plants and trees by eating the bugs that would feed on them, especially in forests and grassland areas, according to study lead author Martin Nyffeler of the University of Basel in Switzerland"
       />
       <Avatar
-        image="https://picsum.photos/160/80"
+        image={imageTwo}
         header="Ceaser salad is actually a smoothie"
         name="Yves"
         link="https://www.britannica.com/plant/tomato#:~:text=tomato%2C%20(Solanum%20lycopersicum)%2C,C%20and%20the%20phytochemical%20lycopene."
@@ -49,7 +52,7 @@ function Avatar(item) {
     <div className="profile-container">
       <div className="container-styles">
         <div className="profile-img">
-          <img src={item.image} alt="" />
+          <img src={item.image} alt=""/>
         </div>
         <div className="header-container">
           <a className="header" href={item.link} target="_blank">
