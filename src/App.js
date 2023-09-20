b98567c39ef49d2475a73fdba4c5ff6a52a9cf5a
Jerrywuon <113319400+JerryWuOn@users.noreply.github.com>
2023-09-20 18:21:04 +0000

    adjust image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { useState } from "react";
 
+
 export default function App() {
   return (
     <div className="App">
@@ -14,7 +15,7 @@ export default function App() {
         information="1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishin"
       />
       <Avatar
-        image="https://picsum.photos/160/80"
+        image="https://files.worldwildlife.org/wwfcmsprod/images/HERO_Penguins_Antarctica/hero_full/29o7jhrgam_Medium_WW267491.jpg/160/80"
         header="Facts about why we eat spaghetti"
         name="Kiiroitori"
         link="https://www.britannica.com/plant/potato"
